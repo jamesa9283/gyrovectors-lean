0c2e972c34045c2810b6a5832f5aeee738fb185e
James Arthur <jamesarthur7@icloud.com>
2020-08-13 22:14:23 +0100

    Updated coop.lean

--- src/basic/coop.lean
+++ src/basic/coop.lean
@@ -1,4 +1,4 @@
-import basic.conventional
+import basic.conventional basic.non_comm
 
 /-
 (The Gyrogroup Cooperation). Let (G, ⊕) be a gyrogroup. The
@@ -14,7 +14,6 @@ a ⊟ b = a⊖gyr[a, b]b
 --class cogyro_group (B : Type)
 
 
---def box_plus: a⊞b := a ⊙ gyr a (⊝b) b
 
 
 /-
@@ -22,3 +21,8 @@ So what do gyrooperation and cogyrooperation take in as inputs and output?
 
 gyrooperation is my a ⊕ b and cogyrooperation is a ⊞ b = a⊕gyr[a, ⊖b]b 
 -/
+
+--open gyrogroup
+
+--variables (G : Type) [gyrogroup G]
+--variables (a b c d : G)
